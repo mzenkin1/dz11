424d10cfba1707586f7223addc30d75b196454d7
Maksim Zenkin <m.a.zenkin@yandex.ru>
2023-04-22 23:40:59 +0300

    summa & subtraction

--- funcs.py
+++ funcs.py
@@ -3,3 +3,9 @@ def sum():
     n2 = int(input("Введите второе положительное число"))
     return n1 + n2
 print(f"сумма равна {sum()}")
+
+def subtraction():
+    n1 = int(input("Введите первое положительное число"))
+    n2 = int(input("Введите второе положительное число"))
+    return n1 - n2
+print(f"Разница равна {subtraction()}")
